2ffef15c34075ef4b8becf5c4ff88ad9210a4967
unknown <alexisreyes90@gmail.com>
2018-08-28 23:51:41 -0400

    had trouble installing bokeh, got it working at last second

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -12,18 +12,18 @@ class Vertex:
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        self.verticies = {}
+        self.vertices = {}
 
     def add_edge(self, start, end):
-        if start not in self.verticies and end not in self.verticies:
+        if start not in self.vertices and end not in self.vertices:
             raise ValueError("No valid entries")
         else:
-            self.verticies[start].add(end)
-            self.verticies[end].add(start)
+            self.vertices[start].add(end)
+            self.vertices[end].add(start)
 
     def add_vertex(self, vertex):
-        if vertex not in self.verticies:
-            self.verticies[vertex] = set()
+        if vertex not in self.vertices:
+            self.vertices[vertex] = set()
         else:
             raise ValueError("Already in the set")
 
@@ -40,5 +40,5 @@ graph.add_vertex(v3)
 # graph.add_edge('3', '4')
 graph.add_edge(v1, v2)
 graph.add_edge(v1, v3)
-print(graph.verticies)
+print(graph.vertices)
 
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+from draw import BokehGraph
+from graph import Graph, Vertex
+
 """
 Demonstration of Graph and BokehGraph functionality.
 """
@@ -8,9 +11,19 @@ from sys import argv
 
 
 def main():
-    pass  # TODO
+    graph = Graph()
+    graph.add_vertex('0')
+    graph.add_vertex('2')
+    graph.add_vertex('5')
+    # graph.add_edge('3', '4')
+    graph.add_edge('0', '2')
+    graph.add_edge('5', '0')
+
+    bg = BokehGraph(graph)
+    bg.show()
 
 
 if __name__ == '__main__':
     # TODO - parse argv
     main()
+    pass
