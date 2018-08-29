0d29908976c96d1f3d32afe6337fe9842bc9816d
Alexander <alexander.botello@g.austincc.edu>
2018-08-28 22:51:08 -0500

    Write random graph function

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -3,14 +3,32 @@
 """
 Demonstration of Graph and BokehGraph functionality.
 """
-
+import random
 from sys import argv
+from graph import Graph
+from draw import BokehGraph
+
+def draw_random_graph(graph, vertices, edges):
+    vertices = random.sample(range(1, 100), vertices)
+    for vertex in vertices:
+        graph.add_vertex(vertex)
+
+    for _ in range(edges):
+        pair = random.choices(vertices, k=2)
+        graph.add_edge(pair[0], pair[1])
+
+    return graph
 
 
 def main():
-    pass  # TODO
+    graph = Graph()
+    graph = draw_random_graph(graph, 30, 60)
+    bg = BokehGraph(graph)
+    bg.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # TODO - parse argv
-    main()
+     main()
+    # graph = Graph()
+    # draw_random_graph(graph, 10, 20)
