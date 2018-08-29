36710645dd9d061101ecabc7326eee957dde5047
Lisa Canini <three_nineteen@yahoo.com>
2018-08-28 20:51:13 -0700

    draw.py implemented

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,40 +2,37 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
-class Vertex:
-    def __init__(self, data):
-        self.data = data
-        self.edges = set()
-    def __repr__(self):
-        return str(self.data)
-
-class Edge:
-    def __init__(self, destination):
-        self.destination = destination
-
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        self.vertices = set()
-    def add_edge(self, start, end):
-        start.edges.add(end)    
-    def add_vertex(self, vertex):
-        self.vertices.add(vertex)
-    def draw_rep(self):
-        rep = ''
-        for v in self.vertices:
-            rep += str(v) + '-> ['
-            for e in v.edges:
-                rep += str(e.data) + ' '
-            rep += ']'
-            print(rep)
-            rep = ''    
+        self.vertices = {}
+
+    def add_vertex(self, vertex, edges=()):
+        if not set(edges).issubset(self.vertices):
+            print('Error')
+        else:
+            self.vertices[vertex] = set(edges)
+
+    def add_edge(self, start, end, bidirectional=True):
+        if start not in self.vertices or end not in self.vertices:
+            print('Cannot connect vertices that are not in the graph')
+            return False
+        elif end in self.vertices[start]:
+            print('Edge already exists')
+            return False
+        else:
+            self.vertices[start].add(end)
+            if bidirectional:
+                self.vertices[end].add(start)
+            return True
+
 
 graph = Graph()  # Instantiate your graph
 graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
 graph.add_vertex('3')
-# graph.add_edge('0', '1')
-# graph.add_edge('0', '3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
 print(graph.vertices)
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -5,11 +5,20 @@ Demonstration of Graph and BokehGraph functionality.
 """
 
 from sys import argv
-
+from graph import Graph
+from draw import BokehGraph
 
 def main():
     pass  # TODO
-
+    graph = Graph()  # Instantiate your graph
+    graph.add_vertex('0')
+    graph.add_vertex('1')
+    graph.add_vertex('2')
+    graph.add_vertex('3')
+    graph.add_edge('0', '1')
+    graph.add_edge('0', '3')
+    bg = BokehGraph(graph)
+    bg.show()
 
 if __name__ == '__main__':
     # TODO - parse argv
