69f688355c10f3f87ab52f3b781774fd8e38e536
Andrew Jarrett <ahrjarrett@gmail.com>
2018-08-29 22:50:14 -0500

    hacky solution for checking if queue contains value in bfs

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -15,29 +15,21 @@ class Graph:
     def __init__(self):
         self.vertices = {}
 
-    def add_vertex(self, vertex, edges=()):
-        # Will this check work given that self.vertices is a dict & not a set?
-        #if not set(edges).issubset(self.vertices)
-        self.vertices[vertex] = set(edges)
+    def add_vertex(self, vertex):
+        self.vertices.update({ vertex: set() })
 
     def add_edge(self, start, end, is_directed = True):
-
-        # Make sure vertices exist in graph:
-        #if start not in self.vertices
-
-        # Make sure edge doesn't already exist
 
         self.vertices[start].add(end)
         if not is_directed:
             self.vertices[end].add(start)
-    
 
 
-v1 = Vertex('A')
-v2 = Vertex('B')
-v3 = Vertex('C')
-g = Graph()
-g.add_vertex(v1)
-g.add_vertex(v2)
-g.add_vertex(v3)
+# v1 = Vertex('A')
+# v2 = Vertex('B')
+# v3 = Vertex('C')
+# g = Graph()
+# g.add_vertex(v1)
+# g.add_vertex(v2)
+# g.add_vertex(v3)
 
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -6,6 +6,7 @@ Demonstration of Graph and BokehGraph functionality.
 
 from random import randint
 from draw import BokehGraph
+from queue import *
 from graph import Graph
 from sys import argv
 
@@ -21,12 +22,53 @@ def draw_random_graph(graph, vertices, edges):
         )
         j += 1
 
+class CheckableQueue(Queue):
+    def contains(self, item):
+        with self.mutex:
+            return item in self.queue
+
+def bfs(graph, start):
+    nodes = []
+    queue = CheckableQueue()
+    queue.put(start)
+
+    while not queue.empty():
+        # print(queue.qsize())
+        curr = queue.get()
+        neighbors = graph.vertices[curr]
+
+        for neighbor in neighbors:
+            if not neighbor in nodes and not queue.contains(neighbor):
+                queue.put(neighbor)
+
+        nodes.append(curr)
+
+    return nodes
+
+
+
 def main():
     graph = Graph()
-    draw_random_graph(graph, 10, 20)
+    #draw_random_graph(graph, 10, 20)
+    #bg = BokehGraph(graph)
+    #bg.show()
 
-    bg = BokehGraph(graph)
-    bg.show()
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_vertex(6)
+    graph.add_edge(1,2)
+    graph.add_edge(1,6)
+    graph.add_edge(2,6)
+    graph.add_edge(2,3)
+    graph.add_edge(3,5)
+    graph.add_edge(1,4)
+    print(bfs(graph, 1))
+
+
+
 
 if __name__ == '__main__':
     # TODO - parse argv
