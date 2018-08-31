a8f39443283a6c7763b1cbd8f33cb44b7daf4a8d
Alexander <alexander.botello@g.austincc.edu>
2018-08-31 16:11:31 -0500

    Implement an iterative dfs algorithm

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -20,7 +20,7 @@ def draw_random_graph(graph, vertices, edges):
         graph.add_edge(pair[0], pair[1], bidirectional=bidirectional)
     return graph
 
-def print_graph( graph ):
+def print_graph(graph):
     for v in graph.vertices:
         line = ""
         line += (str(v) + ": " )
@@ -45,34 +45,74 @@ def breadth_first_search(graph, start_vertex):
         visted.append(vertex)
     return visted
 
+"""Elissa's depth first search"""
+def depth_first_search(graph, start):
+    def dfs_visit(vertex):
+        colors[vertex] = 'gray'
+        nodes.append(vertex)
 
-# def depth_first_search(graph, start):
-#     visited = []
-#     colors = {vertex: "white" for vertex in graph.vertices.keys()}
+        for edge in graph.vertices[vertex]:
+            if colors[edge] == 'white':
+                dfs_visit(edge)
+        colors[vertex] = "black"
 
-#     q = queue.LifoQueue()
-#     q.put(start)
+    nodes = []
+    colors = {vertex : "white" for vertex in graph.vertices.keys()}
+    for vertex in graph.vertices.keys():
+        if colors[vertex] == 'white':
+            dfs_visit(vertex)
+    return nodes
 
-#     while not q.empty():
-#         vertex = q.get()
-#         colors[vertex] = "black"
-#         for edge in graph.vertices[vertex]:
-#             if colors[edge] == "white":
-#                 colors[edge] = "gray"
-#                 q.put(edge)
-#         visited.append(vertex)
-#     return visited
+
+def dfs(graph, start):
+    """
+    Depth first search traversal from a specified
+    starting node
+    """
+    def find_unexplored_edge(vertex):
+        """
+        Helper function that returns an unexplored edge.
+        If all edges are explored the function returns None
+        """
+        for edge in graph.vertices[vertex]:
+            if colors[edge] == "white":
+                colors[edge] = "gray"
+                return edge
+        return None
+
+    visited = []
+    stack = deque()
+    colors = {
+        vertex : "white"
+        for vertex in graph.vertices.keys()
+    }
+
+    stack.append(start)
+    visited.append(start)
+    colors[start] = 'gray'
+
+    while len(stack) > 0:
+        vertex = stack[-1]
+        edge = find_unexplored_edge(vertex)
+        if edge:
+            visited.append(edge)
+            stack.append(edge)
+            continue
+        colors[vertex] = 'black'
+        stack.pop()
+    return visited
+
 
 def main():
     graph = Graph()
     graph = draw_random_graph(graph, 5, 10)
     vertices = list(graph.vertices.keys())
     print_graph(graph)
-    print("bfs: ",breadth_first_search(graph, vertices[0]))
-    print("dfs: ",depth_first_search(graph, vertices[0]))
-
-    # bg = BokehGraph(graph)
-    # bg.show()
+    print('BFS:', breadth_first_search(graph, vertices[0]))
+    print('Elissa DFS: ', depth_first_search(graph, vertices[0]))
+    print("DFS: ", dfs(graph, vertices[0]))
+    bg = BokehGraph(graph)
+    bg.show()
 
 
 if __name__ == "__main__":
